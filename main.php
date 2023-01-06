90a5a018a51865d9ad2602b5cc009fe81f6fb80f
阿循 <105115552+GANSOONLEE@users.noreply.github.com>
2023-01-07 01:14:19 +0800

    Upload Local Files

---
+++ main.php
@@ -0,0 +1,60 @@
+<?php
+
+$db_host ='127.0.0.1';
+$db_user='root';
+$db_pwd='';
+$db_name='user';
+$db_port=3306;
+
+    $con=mysqli_connect($db_host,$db_user,$db_pwd,$db_name,$db_port);
+    if(!$con){
+        die("error:".mysqli_connect_error());
+    }    
+
+    session_start();
+    $userID = $_SESSION['userID'];
+
+    $sql='select userName from user where userID='."'{$userID}';";
+    $res=mysqli_query($con,$sql);
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <ul class="links">
+            <a><li class="link">首頁</li></a>
+            <a><li class="link">測試</li></a>
+            <a><li class="link">測試</li></a>
+        </ul>
+        <div class="logo">
+            <img src="">
+            <p class="username">
+                <?php
+                    if (mysqli_num_rows($res) >= 0) {
+                        while($row = mysqli_fetch_assoc($res)) {
+                            echo $row['userName'];
+                        }
+                    }else{
+                        echo 'unknown';
+                    }
+                ?>
+            </p>
+        </div>
+        <div class="setting">
+            <i class="mode"></i>
+            <i class="setting"></i>
+        </div>
+    </div>
+</html>
---
+++ php/active.php
@@ -0,0 +1,32 @@
+<?php
+
+$db_host ='127.0.0.1';
+$db_user='root';
+$db_pwd='';
+$db_name='user';
+$db_port=3306;
+
+$con=mysqli_connect($db_host,$db_user,$db_pwd,$db_name,$db_port);
+if(!$con){
+    die("error:".mysqli_connect_error());
+}
+
+$verify = stripslashes(trim($_GET['verify']));
+$user = $_GET['userID'];
+$nowtime = time(); 
+
+$query = mysqli_query($con, "select userID,tokenExpire from user where valid='0' and  
+`token`='$verify'"); 
+$row = mysqli_fetch_array($query); 
+if($row){ 
+    if($nowtime<$row['token_expire']){ //24hour 
+        $msg = '您的激活有效期已過，請登錄您的帳號重新發送激活郵件.'; 
+    }else{ 
+        mysqli_query($con, "update user set valid=1 where userID='$user'"); 
+        if(mysqli_affected_rows($con)!=1) die(0); 
+        $msg = '激活成功！'; 
+    } 
+}else{ 
+    $msg = 'error.';     
+} 
+echo $msg; 
